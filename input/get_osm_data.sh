4d9c3f8972499da615537cd21e66421707f6284a
Dan Snow <dsnow@cookcountyassessor.com>
2021-04-14 13:59:22 -0500

    Convert OSM fetch script to work for state

--- input/get_osm_data.sh
+++ input/get_osm_data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Set URLs and filenames for OSM
-export buffer_size="200"
 export osm_url="https://download.geofabrik.de"
 export osm_file="north-america-latest.osm.pbf"
 
@@ -11,24 +10,15 @@ if [ ! -f "$PWD"/shared/"$osm_file" ]; then
     curl "$osm_url"/"$osm_file" --output "$PWD"/shared/"$osm_file"
 fi
 
-# Create a clipped PBF of the buffered county in the shared dir, then symlink
-# it to geography-specific dirs
-for county in $(cat county/temp.txt); do
-    mkdir -p "$PWD"/shared/osm
-    if [ ! -f "$PWD"/shared/osm/"$county".pbf ]; then
-        echo "Creating a clipped PBF of OSM ways for $county county buffer..."
-        clipping_poly="${PWD}/county/buffers/${county}_${buffer_size}.geojson"
+# Create a clipped PBF for each buffered state
+for state in $(cat shared/state_fips.txt); do
+    if [ ! -f "$PWD"/shared/graphs/"$state"/"$state".pbf ]; then
+        echo "Creating a clipped PBF of OSM ways for ${state} state buffer..."
+        clipping_poly="${PWD}/shared/buffers/${state}.geojson"
         osmium extract -p "$clipping_poly" \
             "$PWD"/shared/"$osm_file" \
             --overwrite --progress \
-            -o "$PWD"/shared/osm/"$county".pbf
-    fi
-    echo "Done! Symlinking PBF to tracts/ and zcta/ directories..."
-    if [ ! -L "$PWD"/tract/resources/"$county"/"$county".pbf ]; then
-        ln -s "$PWD"/shared/osm/"$county".pbf "$PWD"/tract/resources/"$county"/"$county".pbf
-    fi
-    if [ ! -L "$PWD"/zcta/resources/"$county"/"$county".pbf ]; then
-        ln -s "$PWD"/shared/osm/"$county".pbf "$PWD"/zcta/resources/"$county"/"$county".pbf
+            -o "$PWD"/shared/graphs/"$state"/"$state".pbf
     fi
 done
 
