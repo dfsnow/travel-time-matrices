94fe5ed39e7804bb7fa055715393fa5d15d32259
Eric Chandler <echandler@cs.uchicago.edu>
2021-05-12 21:37:00 -0500

    write routing matrixes to outputs folder

--- routing/r5r/route_r5r.R
+++ routing/r5r/route_r5r.R
@@ -6,7 +6,7 @@ options(java.parameters = "-Xmx6G")
 
 # Set target county, geography, and env vars
 target_year <- "2010"
-target_state <- "05"
+target_state <- "01"
 target_geography <- "tract"
 target_graph_path <- glue("input/shared/graphs/{target_state}/")
 target_od_path <- glue(
@@ -14,9 +14,15 @@ target_od_path <- glue(
   "origin_destination_by_state/{target_state}/"
 )
 
+# Set trip parameters
+mode <- c("WALK","TRANSIT")
+target_mode <- paste(mode, collapse='-')
+departure_time <- as.POSIXct(strptime("2021-04-16 20:21:17 CDT","%Y-%m-%d %H:%M:%S"))
+departure_time <- Sys.time()
+
 # Load origins and destinations from resources/ dir
 origins <- readr::read_csv(glue(target_od_path, "/origins.csv"))  
-destinations <- readr::read_csv(glue(target_od_path, "/destinations.csv")) 
+destinations <- readr::read_csv(glue(target_od_path, "/destinations.csv"))
 
 # Setup routing path and r5 instance
 r5r_core <- setup_r5(
@@ -31,17 +37,22 @@ mat <- travel_time_matrix(
   r5r_core = r5r_core,
   origins = origins,
   destinations = destinations,
-  mode = c("WALK", "TRANSIT"),
-  departure_datetime = Sys.time(),
-  time_window = 1,
+  mode = mode,
+  departure_datetime = departure_time,
+  time_window = 1, 
   max_walk_dist = Inf,
-  max_trip_duration = 20000,
+  max_trip_duration = 20000, # duration units = minutes
   max_rides = 5,
   verbose = FALSE
 )
 end_time <- Sys.time()
 runtime <- end_time - start_time
-
-# Print total runtime and close r5r instance
+print("Routing matrix finished in :")
 print(runtime)
+
+# Write travel time matrix to csv for later analysis
+out_dir <- glue("output/{target_year}/{target_geography}/travel_time_matrices/{target_state}/{target_mode}")
+if (!dir.exists(out_dir)) dir.create(out_dir, recursive=TRUE)
+write.csv(mat, glue(out_dir, "/ttm.csv"))
+
 stop_r5(r5r_core)
