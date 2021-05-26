6106507235220ea3bf44af2a0974cb9ffb684fc9
Eric Chandler <echandler@cs.uchicago.edu>
2021-05-26 13:04:05 -0500

    api request for feeds properly filter on geography

--- input/get_transit_land_feeds.R
+++ input/get_transit_land_feeds.R
@@ -13,65 +13,64 @@ library(glue)
 
 # Step 1: Get Basic Geographic Bounds  ---------------------------------------
 
-# Load list of US state FIPS codes, ignore territories and PR
-us_states <- unique(tigris::fips_codes$state_code)[1:51]
-
 # Load Transit-Land API key from .Renviron file at root of project directory
 # To get an API key, visit https://www.transit.land/documentation#signing-up-for-an-api-token
-# API key not needed for TransitLand v1 
+# API key not needed for TransitLand v1
 # api_key <- Sys.getenv("TRANSIT_LAND_API_KEY")
 
-# Load state buffers from files
-states_df <- map_dfr(
-  us_states, ~ st_read(glue("input/shared/buffers/", .x, ".geojson"))
-) %>% st_transform(2163)
+# Load lower 48 states' geographies
+us_states <- tigris::states(cb=TRUE) %>%
+  filter(!(STUSPS %in% c('HI','AK','AS','GU','MP','PR','VI'))) %>%
+  st_transform(crs = st_crs(4326)) %>% st_as_sf()
 
-states_outline <- states_df %>% st_union()
-states_bbox <- states_outline %>% st_bbox() 
+# Create bounding box per state
+us_states <- cbind(us_states, sapply(us_states$geometry, st_bbox) %>% t())
 
 # Step 2: Get List Feeds + Metadata ---------------------------------------
 
-# Create API URL bounded to roughly the usa
-feed_locations_url <- paste0(
-  "https://transit.land/api/v1/feeds?per_page=50"
-)
 
-# @NICO How to re-project these coordinates to long/lat?
-bbox_querystring <- paste0("bbox=",
-                           states_bbox$xmin, ",", states_bbox$ymin, ",", 
-                           states_bbox$xmax, ",", states_bbox$ymax)
-
-# Create list to hold feeds
-feed_onestop_ids <- c()
-
-# Feeds results are paginated so we need to make multiple API requests
-while (feed_locations_url) {
-  feed_locations_json <- jsonlite::read_json(feed_locations_url)
-  # TODO: Refactor for-loop to be more R-like
-  for (feed in feed_locations_json$feeds) {
-    # Double-check records are complete (sometimes they're missing coordinates)
-    if (feed$feed_format == "gtfs" && 
-        "geometry" %in% names(feed) &&
-        "coordinates" %in% names(feed$geometry) &&
-        length(feed$geometry$coordinates) > 0) {
-      
-      coords <- unlist(feed$geometry$coordinates)
-      lonlats <- split(coords, 1:2)
-      coords_df <- data.frame(lon = lonlats$`1`, lat = lonlats$`2`)
-      
-      coords_df <- st_as_sf(coords_df, coords = c("lon", "lat"), crs = 4326) %>% st_transform(2163)
-      # @NICO ^^ is this a reasonable projection?
-      
-      # XXX: The feeds metadata  contains USA locations, but this isn't correctly filtering
-      # If any coordinates from this feed intersect the us, keep it.
-      if (any(st_intersects(coords_df, states_outline, sparse=FALSE))) {
-        feed_onestop_ids <- append(feed_onestop_ids, feed$onesteop_id)
+#' Gets gtfs feeds inside a state's bounding box from TransitLand
+query_onestop_ids <- function(xmin, ymin, xmax, ymax) {
+  feed_locations_url <- "https://transit.land/api/v1/feeds?per_page=5"
+  bbox_querystring <- paste0("bbox=",
+                             xmin, ",", ymin, ",",
+                             xmax, ",", ymax)
+  query_url <- paste0(feed_locations_url, "&", bbox_querystring)
+  feed_onestop_ids <- c()
+  # Feeds results are paginated so we need to make multiple API requests
+  while (!is.null(query_url)) {
+    # Wait a few seconds to avoid rate limits?
+    Sys.sleep(runif(1)*2)
+    print(paste("Querying API: ", query_url))
+    # Server may refuse connection
+    feed_locations_json <- tryCatch(jsonlite::read_json(query_url),
+                                    error=function(e){print(e);NULL})
+    if (is.null(feed_locations_json)){
+      return(feed_onestop_ids);
+    }
+    for (feed in feed_locations_json$feeds){
+      inner_feed <- feed %>% flatten()
+      if (inner_feed$feed_format == "gtfs") {
+        feed_onestop_ids <- append(feed_onestop_ids, inner_feed$onestop_id)
       }
     }
+    query_url <- feed_locations_json$meta[['next']]
   }
-  # XXX: Hopefully 'next' evaluates to some falsy value when we reach last result...
-  feed_locations_url <- feed_locations_json$meta[['next']]
+  feed_onestop_ids
 }
+
+
+# Create list to hold feeds
+onestop_ids <- c()
+# TODO: Refactor to tidy-style now that the function is safe
+for (idx in 1:nrow(us_states)) {
+  xmin <- us_states$xmin[idx]
+  ymin <- us_states$ymin[idx]
+  xmax <- us_states$xmax[idx]
+  ymax <- us_states$ymax[idx]
+  onestop_ids <- append(onestop_ids, query_onestop_ids(xmin,ymin,xmax,ymax))
+}
+# state_feeds <- us_states %>% mutate(feeds = query_onestop_ids(xmin,ymin,xmax,ymax))
 
 # Step 3: Get GTFS  ---------------------------------------
 
